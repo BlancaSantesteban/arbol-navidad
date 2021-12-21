f242fece5c997febeba8bffa4287100e8c672c37
Blanca Santesteban <blanca.santesteban@biko2.com>
2021-12-21 13:44:40 +0100

    refactor: naming

--- src/christmasTree.ts
+++ src/christmasTree.ts
@@ -1,9 +1,9 @@
-export function christmasTree(arg0: number) {
-  if (arg0 === 3) {
+export function christmasTree(height: number) {
+  if (height === 3) {
     return '*\n***\n*****';
   }
 
-  if (arg0 == 2) {
+  if (height == 2) {
     return '*\n***';
   }
 
