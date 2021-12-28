8a42e7a9393a8ab1b26e3fb5afec826b11a8399f
Blanca Santesteban <blanca.santesteban@biko2.com>
2021-12-28 12:26:04 +0100

    refactor: extract function

--- src/christmasTree.ts
+++ src/christmasTree.ts
@@ -1,13 +1,19 @@
 export function christmasTree(height: number) {
+  let tree: string = ' '.repeat(height - 1) + '*';
 
-    let tree=' '.repeat(height-1)+"*";
-        for(let i=1;i!=height;i++){
-            if(height-i==1){
-                tree+="\n"+'*'.repeat((i*2)+1);
-                break;
-            }
-            tree+="\n"+' '.repeat((height-1)-i)+'*'.repeat((i*2)+1);
-           
-        }
-    return tree;  
+  for (let i = 1; i !== height; i++) {
+    const isLastFloor = height - i === 1;
+
+    if (isLastFloor) {
+      tree += '\n' + getLeafs(i);
+      return tree;
+    }
+    tree += '\n' + ' '.repeat(height - 1 - i) + getLeafs(i);
+  }
+
+  return tree;
+}
+
+function getLeafs(floor: number) {
+  return '*'.repeat(floor * 2 + 1);
 }
