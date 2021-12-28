7dc466ee7cdbf4d471d062025d17b05d3fe27d59
Blanca Santesteban <blanca.santesteban@biko2.com>
2021-12-28 12:42:52 +0100

    refactor: inline function

--- src/christmasTree.ts
+++ src/christmasTree.ts
@@ -6,16 +6,10 @@ export function christmasTree(height: number) {
       tree += '\n';
     }
 
-    tree += getAir(height, floor) + getLeafs(floor);
+    const air = ' '.repeat(height - 1 - floor);
+    const leafs = '*'.repeat(floor * 2 + 1);
+    tree += air + leafs;
   }
 
   return tree;
-}
-
-function getLeafs(floor: number) {
-  return '*'.repeat(floor * 2 + 1);
-}
-
-function getAir(height: number, floor: number = 0) {
-  return ' '.repeat(height - 1 - floor);
 }
