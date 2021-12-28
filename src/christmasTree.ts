cf22999981913e60da0e3ed45364e5f93448dd13
Blanca Santesteban <blanca.santesteban@biko2.com>
2021-12-28 13:06:51 +0100

    refactor: extract function

--- src/christmasTree.ts
+++ src/christmasTree.ts
@@ -1,5 +1,5 @@
 export function christmasTree(height: number) {
-  const floors = [...Array(height).keys()];
+  const floors = getFloors(height);
 
   return floors.reduce((tree, floor) => {
     const isTopOfTree = floor === 0;
@@ -8,6 +8,12 @@ export function christmasTree(height: number) {
     const leafs = '*'.repeat(floor * 2 + 1);
     return isTopOfTree ? tree + air + leafs : tree + '\n' + air + leafs;
   }, '');
+}
+
+function getFloors(height: number) {
+  // los pisos se contarán de 0 a la altura menos uno
+  // siendo 0 la punta del árbol
+  return [...Array(height).keys()];
 }
 
 /**
